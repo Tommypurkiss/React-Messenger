2403a695426a112c7f1591860802a9c4d75f5ced
Tommy <tommypurkiss1@gmail.com>
2020-02-14 13:58:34 +0000

    first commit to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+// Header
+import HomepageHeader from './homepage-header/HomepageHeader';
+// HomepageContent
+import HomepageContent from './homepage-content/HomepageContent';
+// SignUp Page
+import SignUp from './user-signup/SignUp';
+
+import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      
+        <HomepageHeader />
+
+        <Switch>
+          <Route path ="/" exact component={HomepageContent} />
+          <Route path="/signup" component={SignUp} />
+        </Switch> 
+      </div>
+    </Router>
+
+
   );
 }
+
+
 
 export default App;
---
+++ src/user-signup/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './signup.scss';
+
+function SignUp(){ 
+    return(
+        <div className="signup-content-wrapper">
+            <h1>Sign up here</h1>
+        </div>
+    );
+}
+
+export default SignUp;
